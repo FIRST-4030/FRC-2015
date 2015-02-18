923da4b61cfdaf1c8d9038370b7fdc80c5aca483
Dabo Ross <daboross@daboross.net>
2015-02-17 17:24:09 -0800

    Update tote indexer up/down stuff for new controls/functions

--- src/org/ingrahamrobotics/robot2015/OI.java
+++ src/org/ingrahamrobotics/robot2015/OI.java
@@ -2,11 +2,13 @@ package org.ingrahamrobotics.robot2015;
 
 import org.ingrahamrobotics.robot2015.commands.FixedIndexerShift;
 import org.ingrahamrobotics.robot2015.commands.FullIndexerCollapse;
+import org.ingrahamrobotics.robot2015.commands.IndexerDownAndUp;
 import org.ingrahamrobotics.robot2015.commands.ManualClawControl;
 import org.ingrahamrobotics.robot2015.commands.ManualIndexerControl;
 import org.ingrahamrobotics.robot2015.commands.ToggleClaw;
 import org.ingrahamrobotics.robot2015.commands.VerticalClawMax;
 import org.ingrahamrobotics.robot2015.constants.input.IButton;
+import org.ingrahamrobotics.robot2015.output.Settings;
 
 /**
  * This class is the glue that binds the controls on the physical operator interface to the commands and command groups
@@ -17,8 +19,11 @@ public class OI {
     public OI() {
         IButton.leftClawToggle.getButton().whenPressed(new ToggleClaw(true));
         IButton.rightClawToggle.getButton().whenPressed(new ToggleClaw(false));
-        IButton.indexerUpOneLevel.getButton().whenPressed(new FixedIndexerShift(true));
-        IButton.indexerDownOneLevel.getButton().whenPressed(new FixedIndexerShift(false));
+        IButton.indexerDownOneLevel.getButton().whenPressed(new FixedIndexerShift(null, false));
+        IButton.indexerUpOneLevel.getButton().whenPressed(new FixedIndexerShift(Settings.Key.TOTE_CLEARANCE_ADDITION, false));
+        IndexerDownAndUp downAndUp = new IndexerDownAndUp();
+        IButton.indexerDownAndUp.getButton().whenPressed(downAndUp);
+        IButton.indexerDownAndUp.getButton().whenReleased(downAndUp.getReleasedCommand());
         IButton.indexerCollapseAll.getButton().whenPressed(new FullIndexerCollapse());
         IButton.manualClawHold.getButton().whileHeld(new ManualClawControl());
         IButton.manualIndexerHold.getButton().whileHeld(new ManualIndexerControl());
