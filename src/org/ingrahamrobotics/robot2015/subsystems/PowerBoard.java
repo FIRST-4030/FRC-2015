f42fad361747acf394c74b3ac04fe08392707cf8
Dabo Ross <daboross@daboross.net>
2015-02-17 21:03:29 -0800

    Get the minimum voltage measurement from the past 5 seconds in PowerBoard.

--- src/org/ingrahamrobotics/robot2015/subsystems/PowerBoard.java
+++ src/org/ingrahamrobotics/robot2015/subsystems/PowerBoard.java
@@ -2,9 +2,14 @@ package org.ingrahamrobotics.robot2015.subsystems;
 
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class PowerBoard extends Subsystem {
 
+    private long lastVoltageMeasure = 0;
+    private double minimumVoltage = Double.MAX_VALUE;
+    private final Queue<Double> lastVoltages = new LinkedList<>();
     private final PowerDistributionPanel panel = new PowerDistributionPanel();
 
     @Override
@@ -20,7 +25,23 @@ public class PowerBoard extends Subsystem {
     }
 
     public double getVoltage() {
-        return panel.getVoltage();
+        long now = System.currentTimeMillis();
+        if (now > lastVoltageMeasure + 100) {
+            lastVoltageMeasure = now;
+
+            double currentVoltage = panel.getVoltage();
+            if (currentVoltage <= minimumVoltage) {
+                minimumVoltage = currentVoltage;
+            }
+
+            lastVoltages.add(currentVoltage);
+            if (lastVoltages.size() > 500) {
+                if (lastVoltages.remove() <= minimumVoltage) {
+                    minimumVoltage = lastVoltages.stream().min(Double::compare).get();
+                }
+            }
+        }
+        return minimumVoltage;
     }
 
     public double getTotalEnergy() {
