eeebf4297a8b2c8af95fcf7fc2c03afa22149084
Dabo Ross <daboross@daboross.net>
2015-02-17 20:24:53 -0800

    Use the z axis on the drive joystick for turning - this is for using the handheld controller

--- src/org/ingrahamrobotics/robot2015/constants/input/IAxis.java
+++ src/org/ingrahamrobotics/robot2015/constants/input/IAxis.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.Joystick;
 public enum IAxis {
     driveX(IJoystick.driveJoystick, Joystick.AxisType.kX),
     driveY(IJoystick.driveJoystick, Joystick.AxisType.kY),
-    steer(IJoystick.steeringJoystick, Joystick.AxisType.kX),
+    steer(IJoystick.driveJoystick, Joystick.AxisType.kZ),
     manualControl(IJoystick.secondaryJoystick, Joystick.AxisType.kY);
 
     private Joystick.AxisType axisType;
