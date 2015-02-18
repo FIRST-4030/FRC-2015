5c10288e47f53d3eb11199937542dc047d209ee9
Dabo Ross <daboross@daboross.net>
2015-02-17 18:09:26 -0800

    Output the date the indexer was last reset

--- src/org/ingrahamrobotics/robot2015/commands/ResetIndexerEncoder.java
+++ src/org/ingrahamrobotics/robot2015/commands/ResetIndexerEncoder.java
@@ -1,7 +1,11 @@
 package org.ingrahamrobotics.robot2015.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.ingrahamrobotics.robot2015.Subsystems;
+import org.ingrahamrobotics.robot2015.output.Output;
+import org.ingrahamrobotics.robot2015.output.OutputLevel;
 
 public class ResetIndexerEncoder extends Command {
 
@@ -23,6 +27,7 @@ public class ResetIndexerEncoder extends Command {
         if (lastPressed && !pressed) {
             lastPressed = false;
             Subsystems.indexerEncoder.reset();
+            Output.output(OutputLevel.DEBUG, "indexer-encoder-last-reset-time", new SimpleDateFormat("HH:mm:ss").format(new Date()));
         } else {
             lastPressed = pressed;
         }
